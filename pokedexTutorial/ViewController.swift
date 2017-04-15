1caff1787e43aa12d4eea17de6c1809520a97e80
Matthew Singleton <mattmega4@gmail.com>
2017-04-15 01:11:38 -0700

    added music, button, etc, reorganized structure of groups, plays music and pauses

--- pokedexTutorial/ViewController.swift
+++ pokedexTutorial/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
   
   @IBOutlet weak var collectionView: UICollectionView!
   
   var pokemon = [Pokemon]()
+  var musicPlayer: AVAudioPlayer!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,6 +23,28 @@ class ViewController: UIViewController {
     self.collectionView.dataSource = self
     
     parsePokemonCSV()
+    initAudio()
+  }
+  
+  
+  func initAudio() {
+    
+    let path = Bundle.main.path(forResource: "music", ofType: "mp3")!
+    
+    do {
+      
+      musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path)!)
+      musicPlayer.prepareToPlay()
+      musicPlayer.numberOfLoops = -1
+      musicPlayer.play()
+      
+    } catch let err as NSError {
+      
+      print(err.debugDescription)
+      
+    }
+    
+    
   }
   
   
@@ -54,6 +78,17 @@ class ViewController: UIViewController {
   
   
   
+  @IBAction func musicButtonPressed(_ sender: UIButton) {
+    
+    if musicPlayer.isPlaying {
+      musicPlayer.pause()
+      sender.alpha = 0.2
+    } else {
+      musicPlayer.play()
+      sender.alpha = 1.0
+    }
+    
+  }
   
   
   
