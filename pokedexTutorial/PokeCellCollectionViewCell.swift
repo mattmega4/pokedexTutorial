252c79ebe8149e9e8953de76a529c8934255a1b6
Matthew Singleton <mattmega4@gmail.com>
2017-04-14 23:10:14 -0700

    set up all outlets

---
+++ pokedexTutorial/PokeCellCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  pokedexTutorial
+//
+//  Created by Matthew Singleton on 4/14/17.
+//  Copyright © 2017 Matthew Singleton. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet weak var thumbImg: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  
+  var pokemon: Pokemon!
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    
+    layer.cornerRadius = 5.0
+    
+  }
+  
+  func configureCell(pokemon: Pokemon) {
+    
+    self.pokemon = pokemon
+    
+    nameLabel.text = self.pokemon.name.capitalized
+    thumbImg.image = UIImage.init(named: "\(self.pokemon.pokedexId)")
+    
+  }
+  
+  
+  
+}
--- pokedexTutorial/ViewController.swift
+++ pokedexTutorial/ViewController.swift
@@ -9,14 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.collectionView.delegate = self
+    self.collectionView.dataSource = self
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+} // End of Class
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+extension ViewController: UICollectionViewDelegate {
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    
+  }
+  
+}
+
+
+extension ViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 30
+  }
+  
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCellCollectionViewCell {
       
+      let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
+      
+      cell.configureCell(pokemon: pokemon)
+      
+      return cell
+      
+    } else {
+      return UICollectionViewCell()
     }
+  }
+}
 
 
-
+extension ViewController: UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+    return CGSize(width: 105, height: 105)
+  }
 }
