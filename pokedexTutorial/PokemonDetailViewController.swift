f0d4e62e6863d1917c005726c6d18ed868baf15c
Matthew Singleton <mattmega4@gmail.com>
2017-04-15 17:37:39 -0700

    Added Alamofire and iboulets for detail page and updated model file

--- pokedexTutorial/PokemonDetailViewController.swift
+++ pokedexTutorial/PokemonDetailViewController.swift
@@ -13,8 +13,17 @@ class PokemonDetailViewController: UIViewController {
   var pokemon: Pokemon!
   
   @IBOutlet weak var nameLabel: UILabel!
-  
-  
+  @IBOutlet weak var mainImg: UIImageView!
+  @IBOutlet weak var descriptionLabel: UILabel!
+  @IBOutlet weak var typeLabel: UILabel!
+  @IBOutlet weak var defenseLabel: UILabel!
+  @IBOutlet weak var heightLabel: UILabel!
+  @IBOutlet weak var pokedexLabel: UILabel!
+  @IBOutlet weak var weightLabel: UILabel!
+  @IBOutlet weak var attackLabel: UILabel!
+  @IBOutlet weak var evoLabel: UILabel!
+  @IBOutlet weak var currentEvoImg: UIImageView!
+  @IBOutlet weak var nextEvoImg: UIImageView!
   
   
 
@@ -25,4 +34,8 @@ class PokemonDetailViewController: UIViewController {
     }
 
 
+  @IBAction func backButtonTapped(_ sender: UIButton) {
+    dismiss(animated: true, completion: nil)
+  }
+  
 }
